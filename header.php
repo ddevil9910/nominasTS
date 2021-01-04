c518220df6f27dc7ebc7b08f6a0e3d6a1f2080f2
ddevil9910 <74667660+ddevil9910@users.noreply.github.com>
2021-01-04 16:27:10 -0600

    title

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" href="images/favicon.ico" type="image/ico" />
 
-    <title>Gentelella Alela! | </title>
+  <title><?php if(isset($page_title)) echo $page_title; ?></title>
 
     <!-- Bootstrap -->
     <link href="../plantilla/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
